0b853f29389d1bb303a3e9cf6d1623892ca06b7d
PK2_Stimpy <thefernan1@gmail.com>
2023-02-17 16:09:17 +0100

    Added universe and multiverse repos

--- install.sh
+++ install.sh
@@ -8,4 +8,14 @@ mv /etc/apt/sources.list data/sources.list.bak
 cp data/sources-$DISTRIB_CODENAME.list /etc/apt/sources.list
 apt update
 
+echo "Adding universe repository..."
+add-apt-repository universe
+
+echo "Adding multiverse repository..."
+add-apt-repository multiverse
+
+echo "Updating..."
+
+apt update
+
 echo "Done installing new mirror!"
